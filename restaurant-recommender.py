aa26015b8a6b68f8c3f262520e2aa22cd6e39da4
doug <28678448+drm88@users.noreply.github.com>
2024-02-20 14:29:45 -0500

    added data and fixed spelling

--- restaurant-recommender.py
+++ restaurant-recommender.py
@@ -1,5 +1,7 @@
 #this is a script to recommend a place to eat based off a user input name or food type
+from restaurantData import types, restaurant_data
 
 def welcome():
     print('What would you like to eat? Please enter a type of food: ')
     return
+
