100bf84fcd26b8d2b6b0b5e8b23673a576df5766
doug <28678448+drm88@users.noreply.github.com>
2024-02-20 13:50:25 -0500

    initial commit

---
+++ resturant-recommender.py
@@ -0,0 +1,5 @@
+#this is a script to recommend a place to eat based off a user input name or food type
+
+def welcome():
+    print('What would you like to eat? Please enter a type of food: ')
+    return
